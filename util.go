96c6de9eecc39c3c54493d960c69d5c916fea098
JXSnack <jx.snack@gmail.com>
2025-04-26 00:02:25 +0200

    run on main thread now

--- util.go
+++ util.go
@@ -3,11 +3,13 @@ package vex
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 func setWindowState(flag uint32, allow bool) {
-	if allow {
-		rl.SetWindowState(flag)
-	} else {
-		rl.ClearWindowState(flag)
-	}
+	DoOnRenderThread(func() {
+		if allow {
+			rl.SetWindowState(flag)
+		} else {
+			rl.ClearWindowState(flag)
+		}
+	})
 }
 
 func GetFPS() int {
