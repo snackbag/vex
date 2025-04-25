5a9ff103f473df4d872b1236d117d6fd7795ef9e
JXSnack <jx.snack@gmail.com>
2025-04-25 22:12:45 +0200

    move to style system

--- widget-rect.go
+++ widget-rect.go
@@ -7,17 +7,18 @@ import (
 
 type VRect struct {
 	VBaseWidget
-
-	Color color.RGBA
 }
 
 func (r *VRect) Render() {
-	rl.DrawRectangle(r.X(), r.Y(), r.Width(), r.Height(), r.Color)
+	rl.DrawRectangle(r.X(), r.Y(), r.Width(), r.Height(), r.GetStyleAsColor("background-color"))
 }
 
 func NewRect(color color.RGBA) *VRect {
-	rect := &VRect{Color: color}
+	rect := &VRect{}
 	rect.width = 50
 	rect.height = 50
+
+	rect.SetStyle("background-color", ColorAll(90))
+
 	return rect
 }
