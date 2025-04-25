6ccc609e3761a1fd0d9d52bf4b1936172b40e997
JXSnack <jx.snack@gmail.com>
2025-04-25 23:30:38 +0200

    more type safety

--- stylesheet.go
+++ stylesheet.go
@@ -1,19 +1,34 @@
 package vex
 
 import (
+	"fmt"
+	"github.com/snackbag/vex/extra"
 	"image/color"
 )
 
 type VStyleSheet struct {
 	widgetSpecificStyles map[*VWidget]map[string]interface{}
 	styleClasses         map[string]interface{}
+	typeRegistry         map[string]extra.StyleValueType
 }
 
 func newStyleSheet() *VStyleSheet {
-	return &VStyleSheet{
+	sheet := &VStyleSheet{
 		make(map[*VWidget]map[string]interface{}),
 		make(map[string]interface{}),
+		make(map[string]extra.StyleValueType),
 	}
+
+	// Generic
+	sheet.ReserveType("background-color", extra.SVTColor)
+
+	// Border
+	sheet.ReserveType("border-roundness", extra.SVTFloat32)
+	sheet.ReserveType("border-width", extra.SVTInt)
+	sheet.ReserveType("border-color", extra.SVTColor)
+	sheet.ReserveType("border-segments", extra.SVTInt)
+
+	return sheet
 }
 
 func (s *VStyleSheet) GetKeyRaw(widget *VWidget, key string) interface{} {
@@ -25,13 +40,51 @@ func (s *VStyleSheet) GetKeyRaw(widget *VWidget, key string) interface{} {
 }
 
 func (s *VStyleSheet) SetKey(widget *VWidget, key string, value interface{}) {
+	resTemp := s.GetKeyTypeReservation(key)
+	var res extra.StyleValueType
+	if resTemp == nil {
+		res = extra.GetSVT(value)
+		s.ReserveType(key, res)
+	} else {
+		res = *resTemp
+	}
+
+	if res != extra.GetSVT(value) {
+		if res != extra.GetSVT(extra.ConvertToSVT(res, key, value)) {
+			panic(fmt.Sprintf("cannot set key %s, because it is reserved for a different type", key))
+		}
+	}
+
 	if s.widgetSpecificStyles[widget] == nil {
 		s.widgetSpecificStyles[widget] = make(map[string]interface{})
 	}
 
-	s.widgetSpecificStyles[widget][key] = value
+	s.widgetSpecificStyles[widget][key] = extra.ConvertToSVT(res, key, value)
+}
+
+func (s *VStyleSheet) GetKeyTypeReservation(key string) *extra.StyleValueType {
+	if typ, ok := s.typeRegistry[key]; ok {
+		return &typ
+	}
+	return nil
+}
+
+func (s *VStyleSheet) ReserveType(key string, typ extra.StyleValueType) {
+	s.typeRegistry[key] = typ
 }
 
 func (s *VStyleSheet) GetKeyAsColor(widget *VWidget, key string) color.RGBA {
 	return s.GetKeyRaw(widget, key).(color.RGBA)
+}
+
+func (s *VStyleSheet) GetKeyAsInt(widget *VWidget, key string) int {
+	return s.GetKeyRaw(widget, key).(int)
+}
+
+func (s *VStyleSheet) GetKeyAsFloat32(widget *VWidget, key string) float32 {
+	return s.GetKeyRaw(widget, key).(float32)
+}
+
+func (s *VStyleSheet) GetKeyAsFloat64(widget *VWidget, key string) float64 {
+	return s.GetKeyRaw(widget, key).(float64)
 }
