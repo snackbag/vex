33074466777735e704c4fe5179cc064782dc67b9
JXSnack <jx.snack@gmail.com>
2025-04-25 02:06:10 +0200

    rectangle widget!

---
+++ widget-rect.go
@@ -0,0 +1,23 @@
+package vex
+
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+	"image/color"
+)
+
+type VRect struct {
+	VBaseWidget
+
+	Color color.RGBA
+}
+
+func (r *VRect) Render(process *VProcess) {
+	rl.DrawRectangle(r.X(), r.Y(), r.Width(), r.Height(), r.Color)
+}
+
+func NewRect(color color.RGBA) *VRect {
+	rect := &VRect{Color: color}
+	rect.width = 50
+	rect.height = 50
+	return rect
+}
