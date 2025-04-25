c550e16e860ff8fb71bae7a64f660b918f52ca9a
JXSnack <jx.snack@gmail.com>
2025-04-25 23:30:54 +0200

    border & round

--- widget-rect.go
+++ widget-rect.go
@@ -2,6 +2,7 @@ package vex
 
 import (
 	rl "github.com/gen2brain/raylib-go/raylib"
+	"github.com/snackbag/vex/extra"
 	"image/color"
 )
 
@@ -10,7 +11,22 @@ type VRect struct {
 }
 
 func (r *VRect) Render() {
-	rl.DrawRectangle(r.X(), r.Y(), r.Width(), r.Height(), r.GetStyleAsColor("background-color"))
+	rect := extra.GenerateFloat32Rectangle(r.X(), r.Y(), r.Width(), r.Height())
+	bgColor := r.GetStyleAsColor("background-color")
+	borderRoundness := r.GetStyleAsFloat32("border-roundness")
+	borderWidth := r.GetStyleAsInt("border-width")
+	borderColor := r.GetStyleAsColor("border-color")
+	borderSegments := int32(r.GetStyleAsInt("border-segments"))
+
+	if borderRoundness <= 0 {
+		rl.DrawRectangle(r.X(), r.Y(), r.Width(), r.Height(), bgColor)
+	} else {
+		rl.DrawRectangleRounded(rect, borderRoundness, borderSegments, bgColor)
+
+		if borderWidth > 0 {
+			rl.DrawRectangleRoundedLinesEx(rect, borderRoundness, borderSegments, float32(borderWidth), borderColor)
+		}
+	}
 }
 
 func NewRect(color color.RGBA) *VRect {
@@ -18,7 +34,11 @@ func NewRect(color color.RGBA) *VRect {
 	rect.width = 50
 	rect.height = 50
 
-	rect.SetStyle("background-color", ColorAll(90))
+	rect.SetStyle("background-color", color)
+	rect.SetStyle("border-roundness", 0)
+	rect.SetStyle("border-width", 0)
+	rect.SetStyle("border-color", ColorO(0, 0, 0, 0))
+	rect.SetStyle("border-segments", 1)
 
 	return rect
 }
