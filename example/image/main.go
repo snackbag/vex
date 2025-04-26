5cc4406fcee2b91fe8c88031263c4dd34c0ff029
JXSnack <jx.snack@gmail.com>
2025-04-26 19:23:08 +0200

    allow resize and scale image

--- example/image/main.go
+++ example/image/main.go
@@ -7,9 +7,12 @@ import (
 
 func main() {
 	process := vex.Init("image", 400, 400)
+	process.SetAllowResize(true)
 
 	image := vex.NewImage(path.Join("example", "assets", "image.png"))
-	image.SetSize(362, 211)
+	process.AddUpdateListener(func() { // also gets called on start
+		image.SetSize(process.GetWidth(), 211)
+	})
 	process.AddWidget(image)
 
 	process.Run()
