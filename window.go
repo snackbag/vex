a30806ab68cc5ecc1640514d2ba15aaf11aa8aa1
JXSnack <jx.snack@gmail.com>
2025-04-26 11:22:22 +0200

    font system

--- window.go
+++ window.go
@@ -1,8 +1,10 @@
 package vex
 
 import (
+	"fmt"
 	rl "github.com/gen2brain/raylib-go/raylib"
 	"image/color"
+	"os"
 )
 
 var Process *VProcess = nil
@@ -16,6 +18,7 @@ type VProcess struct {
 	StyleSheet      *VStyleSheet
 
 	widgets []VWidget
+	fonts   map[string]*rl.Font
 }
 
 func (process *VProcess) Show() {
@@ -46,12 +49,32 @@ func (process *VProcess) AddWidget(widget VWidget) {
 	process.widgets = append(process.widgets, widget)
 }
 
+func (process *VProcess) LoadFont(name string, path string, resolution int) {
+	if _, exists := process.fonts[name]; exists {
+		panic(fmt.Sprintf("cannot load font named %s, because there is already a font loaded with the same name", name))
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		rl.TraceLog(rl.LogError, fmt.Sprintf("Couldn't find path '%s' for font '%s' (maybe check working directory?)", name, path))
+	}
+
+	font := rl.LoadFontEx(path, int32(resolution), nil, 0)
+	process.fonts[name] = &font
+}
+
+func (process *VProcess) GetLoadedFont(name string) *rl.Font {
+	if val, ok := process.fonts[name]; ok {
+		return val
+	}
+	return nil
+}
+
 func Init(title string, width int32, height int32) *VProcess {
 	if Process != nil {
 		panic("Cannot create multiple Vex processes")
 	}
 
-	val := &VProcess{title, width, height, ColorAll(255), newStyleSheet(), make([]VWidget, 0)}
+	val := &VProcess{title, width, height, ColorAll(255), newStyleSheet(), make([]VWidget, 0), make(map[string]*rl.Font)}
 	Process = val
 	Process.StyleSheet.widgetSpecificStyles = make(map[*VWidget]map[string]interface{})
 
