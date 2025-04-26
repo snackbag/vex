905af5cfc06d10d212e0f807debb6865f58a011b
JXSnack <jx.snack@gmail.com>
2025-04-26 11:22:31 +0200

    display in arial

--- example/fps_counter/main.go
+++ example/fps_counter/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/snackbag/vex"
+	"path"
 )
 
 func main() {
 	process := vex.Init("FPS Counter", 400, 400)
+	process.LoadFont("arial", path.Join("example", "assets", "Arial.ttf"), 16)
 
 	label := vex.NewLabel("FPS: ?")
+	label.SetStyle("font-name", "arial")
 	process.AddWidget(label)
 
 	vex.DoEvery(1000, func(iteration int) {
