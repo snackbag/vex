303d3b925807c84147ac2d3780cf03085af4a3c8
JXSnack <jx.snack@gmail.com>
2025-04-27 01:09:54 +0200

    call widget events

--- render.go
+++ render.go
@@ -8,6 +8,9 @@ var RenderThreadQueue = make(chan func(), 1024)
 
 func (process *VProcess) startRenderLoop() {
 	isFirstStart := true
+	prevMouseStateLeft := false
+	prevMouseStateMiddle := false
+	prevMouseStateRight := false
 
 	for !rl.WindowShouldClose() {
 		select {
@@ -26,6 +29,36 @@ func (process *VProcess) startRenderLoop() {
 			Process.EventHandler.FireEvent("update")
 		}
 
+		if rl.IsMouseButtonPressed(rl.MouseButtonLeft) && !prevMouseStateLeft {
+			prevMouseStateLeft = true
+			fireEventIfHovered(rl.GetMouseX(), rl.GetMouseY(), "left-click")
+		}
+
+		if rl.IsMouseButtonReleased(rl.MouseButtonLeft) && prevMouseStateLeft {
+			prevMouseStateLeft = false
+			fireEventIfHovered(rl.GetMouseX(), rl.GetMouseY(), "left-release")
+		}
+
+		if rl.IsMouseButtonPressed(rl.MouseButtonMiddle) && !prevMouseStateMiddle {
+			prevMouseStateMiddle = true
+			fireEventIfHovered(rl.GetMouseX(), rl.GetMouseY(), "middle-click")
+		}
+
+		if rl.IsMouseButtonReleased(rl.MouseButtonMiddle) && prevMouseStateMiddle {
+			prevMouseStateMiddle = false
+			fireEventIfHovered(rl.GetMouseX(), rl.GetMouseY(), "middle-release")
+		}
+
+		if rl.IsMouseButtonPressed(rl.MouseButtonRight) && !prevMouseStateRight {
+			prevMouseStateRight = true
+			fireEventIfHovered(rl.GetMouseX(), rl.GetMouseY(), "right-click")
+		}
+
+		if rl.IsMouseButtonReleased(rl.MouseButtonRight) && prevMouseStateRight {
+			prevMouseStateRight = false
+			fireEventIfHovered(rl.GetMouseX(), rl.GetMouseY(), "right-release")
+		}
+
 		rl.BeginDrawing()
 		rl.ClearBackground(process.BackgroundColor)
 
@@ -34,6 +67,14 @@ func (process *VProcess) startRenderLoop() {
 		}
 
 		rl.EndDrawing()
+	}
+}
+
+func fireEventIfHovered(x int32, y int32, event string) {
+	for _, widget := range Process.widgets {
+		if rl.CheckCollisionPointRec(rl.NewVector2(float32(x), float32(y)), widget.GenerateHitbox()) {
+			widget.FireEvent(event)
+		}
 	}
 }
 
